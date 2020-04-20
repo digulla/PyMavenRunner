474c9a25d7f10ac4b5fe31d4f68bdc903ca1d6da
Aaron Digulla <digulla@hepe.com>
2020-04-20 20:20:09 +0200

    Better code to implement a version object. Tests to make sure the code actually works.

--- pmr/version.py
+++ pmr/version.py
@@ -2,23 +2,12 @@
 # -*- coding: utf-8 -*-
 
 class Version:
-    def __init__(self, numbers):
-        self.numbers = numbers
+    def __init__(self, *numbers):
+        self.numbers = tuple(numbers)
     
     def __lt__(self, other):
-        n1 = len(self.numbers)
-        n2 = len(other.numbers)
-        n = max(n1, n2)
-        
-        for i in range(0, n):
-            v1 = self.numbers[i] if i < n1 else -1
-            v2 = other.numbers[i] if i < n2 else -1
-            
-            if v1 > v2:
-                return False
-        
-        return True
-            
+        return self.numbers < other.numbers
+
     def __eq__(self, other):
         return self.numbers == other.numbers
 
---
+++ tests/test_version.py
@@ -0,0 +1,49 @@
+#!python3
+# -*- coding: utf-8 -*-
+
+from pmr.version import Version
+
+def test_same():
+    v1 = Version(1)
+    v2 = Version(1)
+    assert v1 == v2
+
+def test_same2():
+    v1 = Version(1,1)
+    v2 = Version(1,1)
+    assert v1 == v2
+
+def test_before():
+    v1 = Version(1)
+    v2 = Version(2)
+    assert v1 < v2
+
+def test_before2():
+    v1 = Version(1,0)
+    v2 = Version(2,0)
+    assert v1 < v2
+
+def test_before3():
+    v1 = Version(1,9)
+    v2 = Version(2,0)
+    assert v1 < v2
+
+def test_different_length():
+    v1 = Version(1)
+    v2 = Version(2,0)
+    assert v1 < v2
+
+def test_different_length2():
+    v1 = Version(1,0)
+    v2 = Version(2)
+    assert v1 < v2
+
+def test_after():
+    v1 = Version(2)
+    v2 = Version(1)
+    assert v1 > v2
+
+def test_after2():
+    v1 = Version(2,0)
+    v2 = Version(1,9)
+    assert v1 > v2
