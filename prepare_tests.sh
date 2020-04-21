b125cf5a23b15f6edc5de2555ac99f2d96f24b54
Aaron Digulla <digulla@swissquant.com>
2020-04-21 16:42:42 +0200

    pyenv is useful for development but too much to force on end users. Just make sure the right version of Python is around before commencing. Let the user figure out how to fix the path.

--- prepare_tests.sh
+++ prepare_tests.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-type pyenv >& /dev/null || {
-	echo "Please install pyenv: https://github.com/pyenv/pyenv#installation"
-	exit 2
+python -c $'import sys\nif sys.version_info[0:2] < (3,7):\n    sys.exit(1)\n' || {
+	echo "You need at least Python 3.7 to run this."
+	echo "Please install it from https://www.python.org/downloads/"
+	echo "or use pyenv: https://github.com/pyenv/pyenv#installation"
+	exit 1
 }
+
 type pipenv >& /dev/null || {
 	echo "Please install pipenv: https://pipenv.pypa.io/en/latest/"
 	exit 2
 }
-
-pyenv install --skip-existing 3.7.7 || exit 1
-pyenv rehash
-eval "$(pyenv init -)"
-pyenv shell 3.7.7
-
-#pyenv version
-#python --version
 
 pipenv install || exit 1
 
