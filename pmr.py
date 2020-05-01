4ca0419594d4ec9d45a93a0e417076da1b592c0e
Aaron Digulla <digulla@hepe.com>
2020-05-01 13:50:50 +0200

    Set application name and version

--- pmr.py
+++ pmr.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from pmr.ui import MainWindow
+import pmr
 
 if __name__ == '__main__':
     import sys
     from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtCore import QCoreApplication
 
     app = QApplication(sys.argv)
+    QCoreApplication.setOrganizationName('de.pdark')
+    QCoreApplication.setApplicationName('PyMavenRunner')
+    QCoreApplication.setApplicationVersion(pmr.VERSION)
 
     mainWindow = MainWindow()
     mainWindow.show()
